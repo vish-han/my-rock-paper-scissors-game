91f16215185458d724a05431b1ed484b1e58efce
Vishal Chauhan <vshlchauhab@gmail.com>
2021-09-25 22:36:59 +0530

    contest display added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 const pickuserHand=(hand)=>{
     console.log(hand)
+    let hands=document.querySelector(".hands");
+    hands.style.display="none";
+    let contest=document.querySelector(".contest")
+    contest.style.display="flex";
+  
 }
